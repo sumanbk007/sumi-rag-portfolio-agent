ae391d521cb7693410e6b3663e5f816fe2f2b772
Suman <suman@krispcallmail.com>
2026-08-13 10:38:53 +0545

    Added the domain and fix the cors error

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,7 +7,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "models/text-embedding-004")
 CHAT_MODEL = os.getenv("CHAT_MODEL", "gemini-2.5-flash")
 ALLOWED_ORIGINS = os.getenv(
-    "ALLOWED_ORIGINS", "http://localhost:3000"
+    "ALLOWED_ORIGINS", "http://localhost:3000","https://sumi-rag-portfolio-agents.vercel.app","https://www.sumanbk.com.np"
 ).split(",")
 INDEX_DIR = os.getenv("INDEX_DIR", "./app/data/index")
 KB_DIR = os.getenv("KB_DIR", "./app/data/knowledge_base")
